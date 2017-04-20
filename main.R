f77a03d005668b585691180c4cdbcb8bc0b9e514
AdonRain <leids@foxmail.com>
2017-04-20 21:43:25 +0800

    first commit

---
+++ main.R
@@ -0,0 +1,7 @@
+import <- function (pkgs){
+  new_pkgs <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
+
+  if (length(new_pkgs)) install.packages(new_pkgs, dependencies = TRUE)
+
+  sapply(pkgs, require, character.only = TRUE)
+}
